a6efe0ff59ed3bf31a888f81c6a3b753fb3bcae6
zauqcollection2-ship-it <zauqcollection2@gmail.com>
2026-08-04 23:24:51 +0500

    Update config.ts
    fix build errors

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -1,7 +1,5 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,7 +12,5 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth };
