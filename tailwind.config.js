fb8ebaacd769a8a9eb5c075dfd25d6049100d0c5
zauqcollection2-ship-it <zauqcollection2@gmail.com>
2026-08-04 23:20:59 +0500

    Update tailwind.config.js
    fix build errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        gold: {
-          50: '#F9F3E8',
-          100: '#F0E3CC',
-          200: '#E1C799',
-          300: '#D2AB66',
-          400: '#C39133',
+        yellow: {
           500: '#C9A84C',
-          600: '#B8973A',
-          700: '#A7862A',
-          800: '#96751A',
-          900: '#85640A',
         },
       },
     },
