6bf46bbfb2bf93bf4b7126f96c85e3c91d18505f
zauqcollection2-ship-it <zauqcollection2@gmail.com>
2026-08-04 23:19:26 +0500

    Update next.config.js
    fix build errors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-  },
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
