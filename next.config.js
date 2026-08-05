1ad104e0c66a161b5d151f979721561a28fe7dd9
zauqcollection2-ship-it <zauqcollection2@gmail.com>
2026-08-05 08:13:11 +0500

    Update next.config.js
    fix firebase import and webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    };
+    return config;
+  },
+}
 
 module.exports = nextConfig;
